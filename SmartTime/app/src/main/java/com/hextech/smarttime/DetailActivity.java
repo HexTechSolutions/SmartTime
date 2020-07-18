7ed9353959b54f01bdab33c20dfd683daecee5da
NisalM <nmmuthugala@gmail.com>
2020-07-18 16:11:29 +0530

    LN-7: Create UI for DetailActivity -Adding back button

--- SmartTime/app/src/main/java/com/hextech/smarttime/DetailActivity.java
+++ SmartTime/app/src/main/java/com/hextech/smarttime/DetailActivity.java
@@ -2,6 +2,7 @@ package com.hextech.smarttime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class DetailActivity extends AppCompatActivity {
@@ -10,5 +11,16 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        assert getSupportActionBar() != null; //null check
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //show back button
+    }
+
+    //Back to the main page
+    @Override
+    public boolean onSupportNavigateUp(){
+        Intent myIntent = new Intent(DetailActivity.this, MainActivity.class);
+        DetailActivity.this.startActivity(myIntent);
+        return true;
     }
 }
