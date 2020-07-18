c9e4d02033c98c10af71627d434adea02a2a0251
Charana Mayakaduwa <charana2010@gmail.com>
2020-07-18 17:15:34 +0530

    LN-6: Implemented ToDoList using recyclerView

--- SmartTime/app/src/main/java/com/hextech/smarttime/ToDoListActivity.java
+++ SmartTime/app/src/main/java/com/hextech/smarttime/ToDoListActivity.java
@@ -1,14 +1,29 @@
 package com.hextech.smarttime;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class ToDoListActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    String listArray[], descriptionArray[];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_list);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        listArray = getResources().getStringArray(R.array.to_do_list);
+        descriptionArray = getResources().getStringArray(R.array.to_do_list_description);
+
+        ToDoListViewAdapter toDoListViewAdapter = new ToDoListViewAdapter(this, listArray, descriptionArray);
+        recyclerView.setAdapter(toDoListViewAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
