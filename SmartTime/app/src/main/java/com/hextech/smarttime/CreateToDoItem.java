715439fe01b7aca6b801743414ff89ff8a9a942e
Charitha Rajapakse <charithacj5@gmail.com>
2020-07-18 14:47:03 +0530

    Dropdown Button Coding

--- SmartTime/app/src/main/java/com/hextech/smarttime/CreateToDoItem.java
+++ SmartTime/app/src/main/java/com/hextech/smarttime/CreateToDoItem.java
@@ -5,13 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class CreateToDoItem extends AppCompatActivity {
 
