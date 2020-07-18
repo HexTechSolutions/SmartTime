78ecb5ede5931aeb23fc7d37c0be237290439571
Charitha Rajapakse <charithacj5@gmail.com>
2020-07-18 13:25:26 +0530

    Adding Back Button to the Home Page

--- SmartTime/app/src/main/java/com/hextech/smarttime/CreateToDoItem.java
+++ SmartTime/app/src/main/java/com/hextech/smarttime/CreateToDoItem.java
@@ -2,6 +2,7 @@ package com.hextech.smarttime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class CreateToDoItem extends AppCompatActivity {
@@ -10,5 +11,16 @@ public class CreateToDoItem extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_to_do_item);
+
+        assert getSupportActionBar() != null;   //null check
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);   //show back button
     }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        Intent myIntent = new Intent(CreateToDoItem.this, MainActivity.class);
+        CreateToDoItem.this.startActivity(myIntent);
+        return true;
+    }
+
 }
