e99776114727fbbaa79cb39e04715f8a2077afcc
DESKTOP-3QK4ISF\Ishan <ishanw20@gmail.com>
2020-07-21 21:02:22 +0530

    LN-16: Implement Push Notifications - Investigations

--- SmartTime/app/src/main/java/com/hextech/smarttime/MainActivity.java
+++ SmartTime/app/src/main/java/com/hextech/smarttime/MainActivity.java
@@ -1,8 +1,14 @@
 package com.hextech.smarttime;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,17 +17,25 @@ public class MainActivity extends AppCompatActivity {
 
     private Button viewListButton, addToDoItemButton;
 
+    public static final String NOTIFICATION_CHANNEL_ID = "10001";
+    private final static String default_notification_channel_id = "default";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createNotificationChannel();
+
         viewListButton = findViewById(R.id.viewEvents);
         addToDoItemButton = findViewById(R.id.addEvent);
 
         viewListButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                 openViewTodoList();
+                showNotification();
+                openViewTodoList();
+
             }
         });
 
@@ -33,8 +47,38 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void openViewTodoList(){
+    public void openViewTodoList() {
         Intent intent = new Intent(this, ToDoListActivity.class);
+
         startActivity(intent);
+    }
+
+
+    private void createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+
+    NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("Sexxx")
+            .setContentText("Sexxxx")
+            .setStyle(new NotificationCompat.BigTextStyle()
+                    .bigText("More Sexxxx"))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+    ;
+
+    public void showNotification() {
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(000001, builder.build());
     }
 }
