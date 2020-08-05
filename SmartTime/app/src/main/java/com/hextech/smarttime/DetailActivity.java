79bd332f436d502fc0975ca39c7c5220b146f487
DESKTOP-3QK4ISF\Ishan <ishanw20@gmail.com>
2020-08-05 23:05:57 +0530

    Populate Detail Activity When Nativating from Notification. Nearest date first

--- SmartTime/app/src/main/java/com/hextech/smarttime/DetailActivity.java
+++ SmartTime/app/src/main/java/com/hextech/smarttime/DetailActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,35 +14,57 @@ import com.hextech.smarttime.util.ToDoItem;
 import com.hextech.smarttime.util.Utilities;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 
 public class DetailActivity extends AppCompatActivity {
 
     TextView taskTitleTF, taskDescriptionTF, taskCategoryTF, taskDateTF;
     Button deleteTaskBtn;
     ToDoItem item;
+    String title,description,category,date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        Bundle extras = getIntent().getExtras();
-        String Lon,Lat;
+        taskTitleTF = findViewById(R.id.taskTitleTF);
+        taskDescriptionTF = findViewById(R.id.taskDescriptionTF);
+        taskCategoryTF = findViewById(R.id.taskCategoryTF);
+        taskDateTF = findViewById(R.id.taskDateTF);
+        deleteTaskBtn = findViewById(R.id.DeleteTaskBtn);
 
-        if(extras != null){
-            Lon = extras.getString("longitude");
-            Lat = extras.getString("latitude");
+        Intent intent = getIntent();
+        String category = intent.getStringExtra("category");
+
+        //Only Runs when navigating from Notification
+        if(category != null){
+            ArrayList data = DBHelper.getAllData(getApplicationContext());
+            ArrayList<ToDoItem> CategoryItems = new ArrayList<>();
+
+            System.out.println(category);
+            for (int i = 0;  i < data.size(); i++){
+                ToDoItem td = (ToDoItem) data.get(i);
+
+                if(td.getCategory().equals(category)){
+                    CategoryItems.add(td);
+                }
+            }
+
+            Collections.sort(CategoryItems);
+            Log.i("SmartTime", "Sorted");
+
+            taskTitleTF.setText(CategoryItems.get(0).getTitle());
+            taskDescriptionTF.setText(CategoryItems.get(0).getDescription());
+            taskCategoryTF.setText(CategoryItems.get(0).getCategory());
+            taskDateTF.setText(CategoryItems.get(0).getDueDate().toString());
         }
 
         assert getSupportActionBar() != null; //null check
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //show back button
 
-        taskTitleTF = findViewById(R.id.taskTitleTF);
-        taskDescriptionTF = findViewById(R.id.taskDescriptionTF);
-        taskCategoryTF = findViewById(R.id.taskCategoryTF);
-        taskDateTF = findViewById(R.id.taskDateTF);
 
-        deleteTaskBtn = findViewById(R.id.DeleteTaskBtn);
 
         final int recordId = getIntent().getIntExtra("itemNumber", -1);
         if (recordId != -1) {
